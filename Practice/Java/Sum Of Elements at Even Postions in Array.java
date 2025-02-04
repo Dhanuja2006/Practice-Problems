447803357cdbf4a366fb79a34c506d514570a005
Dhanuja2006 <dhanujavidyat@gmail.com>
2025-02-04 08:29:52 +0530

    Create Sum Of Elements at Even Postions in Array.java

---
+++ Practice/Java/Sum Of Elements at Even Postions in Array.java
@@ -0,0 +1,22 @@
+import java.util.*;
+//Sum of all elements at even position & sum of all odd elements in an array
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int sum_even = 0;
+	    int oddEleSum = 0;
+	    
+	    int n = sc.nextInt();
+	    int arr[] = new int[n];
+	    
+	    for(int i = 0; i < n; i++){
+	        arr[i] = sc.nextInt();
+	    }
+	    
+	    for(int i = 0; i < n; i++){
+	        if(i%2 == 0) //Sum of elements at even positions
+	            sum_even += arr[i];
+	    System.out.println("Sum of elements at even positions: "+ sum_even + "\nSum of all odd elements in an array: " + oddEleSum);
+	}
+}
