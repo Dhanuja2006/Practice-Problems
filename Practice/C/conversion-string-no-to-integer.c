d6b80f531e6a4230d8f9c68a01d9338757b880dc
Dhanuja2006 <dhanujavidyat@gmail.com>
2025-02-26 11:07:51 +0530

    Update conversion-string-no-to-integer.c

--- Practice/C/conversion-string-no-to-integer.c
+++ Practice/C/conversion-string-no-to-integer.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+/*
 void main(){
   char a = '2';
   int b = (int)a - '0';
   printf("%d", b);
+}
+*/
+
+int main()
+{
+    char a[] = "123";
+    int x = 0;
+    for(int i = 0; i < (sizeof(a)/sizeof(a[0])) - 1; i++){
+        int no = (int)a[i] - '0';
+        x = x*10 + no;
+    }
+    printf("%d", x);
+    return 0;
 }
