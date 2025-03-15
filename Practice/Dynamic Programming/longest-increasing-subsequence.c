92407454ab94cb4b5a30893d720d39df8652605a
Dhanuja2006 <dhanujavidyat@gmail.com>
2025-03-15 08:45:07 +0530

    Update longest-increasing-subsequence.c

--- Practice/Dynamic Programming/longest-increasing-subsequence.c
+++ Practice/Dynamic Programming/longest-increasing-subsequence.c
@@ -1,1 +1,20 @@
-x
+int lengthOfLIS(int* nums, int numsSize) {
+    int dp[numsSize];
+    for(int i = 0; i < numsSize; i++){
+        dp[i] = 1;
+    }
+    for(int i = 0; i < numsSize; i++){
+        for(int j = 0; j < i; j++){
+            if( nums[j] < nums[i]){
+                dp[i] = (dp[i] < dp[j] + 1) ? dp[i] = dp[j] + 1 : dp[i];
+            }
+        }
+    }
+    int max = 0;
+    for(int i = 0; i < numsSize; i++){
+        if(dp[i] > max){
+            max = dp[i];
+        }
+    }
+    return max;
+}
