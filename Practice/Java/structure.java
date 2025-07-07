43d9e6336fc53a38d574a11a84831998e4fc705c
Dhanuja2006 <dhanujavidyat@gmail.com>
2025-07-07 14:08:20 +0530

    Create structure.java

---
+++ Practice/Java/structure.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+    }
+}
+
+/*
+1) main method is entry point of the program. Hence it has to be in pulic else entry will be blocked
+2) when private is used instead of public: can't find main(String[]) method in class: Main
+3) void is used cuz main is not gonna return any value to other variable.
+*/
